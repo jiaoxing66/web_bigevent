a26f134d2f23caa8c6b7a54d18a36c8bc1167deb
[jiaoxing66] <[815600375@qq.com]>
2021-12-30 23:48:27 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,17 @@
 $.ajaxPrefilter(function (options) {
   // 再发起真正的  ajax请求之前  统一请求的根路径
   options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+  // 统一为有权限的接口， 设置 headers 请求头
+  if (options.url.indexOf('/my') !== -1) {
+    options.headers = { Authorization: localStorage.getItem('token') || '' }
+  }
+
+  // 全局挂在没有token强制跳转
+  options.complete = function (res) {
+    // console.log(res)
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      localStorage.removeItem('token')
+      location.href = 'login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+$(function () {
+  getUserInfo() // 获取用户信息
+  // 退出登录
+  $('#btnLogout').on('click', function () {
+    var layer = layui.layer
+    layer.confirm('确定退出吗？', { icon: 3, title: '提示' }, function (index) {
+      // 清空本地存储
+      localStorage.removeItem('token')
+      // 跳转到登录页
+      location.href = '/login.html'
+      // 关闭confirm询问框
+      layer.close(index)
+    })
+  })
+})
+
+// 获取用户基本信息
+function getUserInfo() {
+  $.ajax({
+    type: 'get',
+    url: '/my/userinfo',
+    // headers: { Authorization: localStorage.getItem('token') || '' },
+    success: function (res) {
+      if (res.status !== 0) {
+        return layui.layer.msg('获取用户信息失败')
+      }
+      // 调用 renderAvatar 渲染用户图象
+      renderAvatar(res.data)
+    }
+    // 不论成功还是失败都会执行这个
+    /* complete: function (res) {
+      // console.log(res)
+      if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        localStorage.removeItem('token')
+        location.href = 'login.html'
+      }
+    } */
+  })
+}
+
+// 渲染用户图象
+function renderAvatar(user) {
+  var name = user.username || user.nickname
+  // $('#welcome').html('欢迎' + name)
+  $('#welcome').html(`欢迎 ${name}`) // 渲染用户名字
+  // 渲染用户图象
+  if (user.user_pic !== null) {
+    $('.text-avatar').hide()
+    $('.layui-nav-img').attr('url', user.user_pic)
+  } else {
+    $('.layui-nav-img').hide()
+    var first = name.slice(0, 1).toUpperCase()
+    $('.text-avatar').html(first)
+  }
+}
